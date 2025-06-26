a1d94e1213195ab8d6b2658db49f1346ae4ec95b
ZAvrikDinozavrik <zaz965@stm32f0.ru>
2025-06-26 22:15:23 +0300

    Проведён рефакторинг кода

--- PiWAVPlayer.py
+++ PiWAVPlayer.py
@@ -1,16 +1,23 @@
-import wave
-import pyaudio
+'''
+Player for sounds in WAV-formats
+'''
+
 import logging
 import time
-import numpy as np
-from typing import Optional
+import wave
 from ctypes import CFUNCTYPE, c_char_p, c_int, cdll
+from typing import Optional, Tuple, Mapping
+
+import numpy as np
+import pyaudio
 
 ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 
 
-def py_error_handler(filename, line, function, err, fmt):  # block logs from asound
-    pass
+def py_error_handler(_filename, _line, _function, _err, _fmt):
+    '''
+    block logs from asound
+    '''
 
 
 c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
@@ -19,9 +26,14 @@ asound.snd_lib_error_set_handler(c_error_handler)
 
 
 class PiWAVPlayer:
-    '''Player for wav files'''
+    '''A simple WAV player class'''
 
-    def __init__(self):
+    def __init__(self) -> None:
+        '''
+        Initialize WAV player
+
+        This sets up PyAudio for playback and initializes necessary components.
+        '''
         self._p = pyaudio.PyAudio()
         self._audiostream: Optional[pyaudio.Stream] = None
         self._filestream: Optional[wave.Wave_read] = None
@@ -32,7 +44,15 @@ class PiWAVPlayer:
         self._logger = None
 
     def load(self, filename: str) -> bool:
-        '''Loads wav files'''
+        '''
+        Load a WAV file into the player.
+
+        Args:
+            filename (str): The path to the WAV file to be loaded.
+
+        Returns:
+            bool: True if loading was successful, False otherwise.
+        '''
         try:
             self._filestream = wave.open(filename, 'rb')
             self._filename = filename
@@ -42,8 +62,25 @@ class PiWAVPlayer:
                 self._logger.error(f'File has not loaded! Error: {str(e)}')
             return False
 
-    def audio_stream_callback(self, in_data, frame_count, time_info, status):
-        '''Callback funtion for playback using PyAudio'''
+    def audio_stream_callback(
+        self,
+        _in_data: bytes | None,
+        frame_count: int,
+        _time_info: Mapping[str, float],
+        _status: int,
+    ) -> Tuple[bytes | None, int]:
+        '''
+        Callback function for playback using PyAudio.
+
+        Args:
+            _in_data (bytes | None): Input data from previous buffer.
+            frame_count (int): Number of frames to process.
+            _time_info (Mapping[str, float]): Time information passed by ASound.
+            _status (int): Status code passed by ASound.
+
+        Returns:
+            Tuple[bytes | None, int]: Data to send back and type of data requested.
+        '''
         if self._filestream:
             start_pos = self._position
             end_pos = start_pos + frame_count
@@ -62,7 +99,12 @@ class PiWAVPlayer:
         return (None, pyaudio.paCanNotReadFromACallbackStream)
 
     def play(self) -> None:
-        '''Starts playback of loaded file'''
+        '''
+        Start playback of the loaded file.
+
+        This method ensures that a file is loaded before attempting playback.
+        It handles both normal and looped playback modes.
+        '''
         if self._filestream is None:
             if not self._filename:
                 if self._logger:
@@ -94,15 +136,21 @@ class PiWAVPlayer:
                 time.sleep(0.1)
         except KeyboardInterrupt:
             pass
-        self.stop(type='standart')
+        self.stop(req_type='standart')
 
-    def stop(self, type: str = '') -> None:
-        '''Stops playback'''
+    def stop(self, req_type: str = '') -> None:
+        '''
+        Stop playback.
+
+        Args:
+            req_type (str, optional): Type of request to send back.
+            Defaults to 'standard'.
+        '''
         if self._audiostream:
             self._audiostream.stop_stream()
             self._audiostream.close()
             self._audiostream = None
-        elif type not in ('standart', 'terminate'):
+        elif req_type not in ('standart', 'terminate'):
             if self._logger:
                 self._logger.warning('Not a single file is playing!')
 
@@ -110,16 +158,24 @@ class PiWAVPlayer:
             self._filestream.close()
             self._filestream = None
             self._filename = ''
-        elif type not in ('standart', 'terminate'):
+        elif req_type not in ('standart', 'terminate'):
             if self._logger:
                 self._logger.warning('Not a single file is loaded!')
 
-        if type != 'terminate':
+        if req_type != 'terminate':
             if self._logger:
                 self._logger.info('Playback is stopped')
 
     def set_volume(self, volume: float) -> float:
-        '''Sets volume level'''
+        '''
+        Set the volume level.
+
+        Args:
+            volume (float): Volume value between 0.0 and 1.0
+
+        Returns:
+            float: The new volume level
+        '''
         if 0 <= volume <= 1:
             self._volume = volume
             return self._volume
@@ -128,26 +184,49 @@ class PiWAVPlayer:
         return -1
 
     def get_volume(self) -> float:
-        '''Gets volume level'''
+        '''
+        Get the current volume level.
+
+        Returns:
+            float: The current volume level (between 0.0 and 1.0)
+        '''
         return self._volume
 
     def set_loop_mode(self, is_loop: bool) -> bool:
-        '''Sets play mode'''
+        '''
+        Set play mode to loop or not.
+
+        Args:
+            is_loop (bool): True if looping should occur, False otherwise
+
+        Returns:
+            bool: The new loop state
+        '''
         self._is_loop = is_loop
         return self._is_loop
 
     def get_loop_mode(self) -> bool:
-        '''Gets play mode'''
+        '''
+        Get the current play loop mode.
+
+        Returns:
+            bool: True if playing in loop mode, False otherwise
+        '''
         return self._is_loop
 
     def set_logger(self, logger: logging.Logger) -> None:
-        '''Sets play mode'''
+        '''
+        Set the logger for this player instance.
+        '''
         self._logger = logger
-        return
 
     def __del__(self):
-        '''Free PyAudio'''
-        self.stop(type='terminate')
+        '''
+        Cleanup method called when the object is garbage collected.
+
+        This stops playback and terminates PyAudio.
+        '''
+        self.stop(req_type='terminate')
         self._p.terminate()
 
 
@@ -168,6 +247,5 @@ if __name__ == '__main__':
 
     player.set_logger(logging.getLogger())
 
-    is_loaded = player.load(args.filename)
-    if is_loaded:
+    if player.load(args.filename):
         player.play()
